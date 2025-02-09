ce667eda2d901b0abfccaeececcfd13bc7d24627
Santiago Cuadros Mateus <79917786+sacuadros09@users.noreply.github.com>
2025-02-09 13:56:05 -0500

    Cuadros

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello(request) :
-    return HttpResponse ("Hello World")
+    return HttpResponse ("<h1>Hello World</h1>")
+
+def about(request) :
+    return HttpResponse ("<h2>About</h2>")
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import hello
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", hello)
+    path("", views.hello),
+    path("about/", views.about)
 ]
